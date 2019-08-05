19262e16e0629f7cab5bb524e8ab06ca0b48007e
Ronak Bokaria <ronak.bokaria@sunarchtechnologies.com>
2019-08-05 09:43:37 +0530

    mysql driver and migration creation

---
+++ app/Listeners/Http.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const Database = use('Database')
+
+const Http = (exports = module.exports = {})
+
+Http.method = async () => {}
+
+Http.onStart = function() {
+  Database.on('query', console.log)
+  Database.on('sql', console.log)
+}
---
+++ database/migrations/1564917613421_short_urls_schema.js
@@ -0,0 +1,41 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ShortUrlsSchema extends Schema {
+  up() {
+    this.create('short_urls', (table) => {
+      table
+        .bigInteger('_id')
+        .unsigned()
+        .notNullable()
+        .increments()
+
+      table.string('partition_index', 1).nullable()
+      table.string('special_url', 255).nullable()
+      table.string('short_url', 1024).nullable()
+      table.string('original_url', 1024).nullable()
+      table.date('expire_date', 1).nullable()
+      table.time('expire_time', 1).nullable()
+      table.string('user_name', 1).nullable()
+      table.string('user_email', 1).nullable()
+      table.string('user_mobile', 1).nullable()
+      table.string('user_country', 1).nullable()
+      table.string('url_meta', 1).nullable()
+      table.string('url_category', 1).nullable()
+      table.string('display_ads', 1).nullable()
+      table.string('analytic_report', 1).nullable()
+      table.string('force_disabled', 1).nullable()
+      table.string('created_at', 1).nullable()
+      table.string('created_at', 1).nullable()
+      table.string('deleted_at', 1).nullable()
+    })
+  }
+
+  down() {
+    this.drop('short_urls')
+  }
+}
+
+module.exports = ShortUrlsSchema
